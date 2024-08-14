c0a2436f94683565a204e0e9285b0e8c6a16977b
bino kochumol varghese <binomathewvarghese@gmail.com>
2024-08-14 14:00:09 +0300

    App deployed in firebase and read me updated.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
+### JOb Summary App
+A dashboard application that provides real-time job summaries for employees, automatically refreshing every 30 seconds for up-to-date information.
+
+#### Link
+https://react-job-dashboard.web.app/
 
 
-- Total appointments, Consulted patients, Patients waiting, Pending Surgeries 
-- #, Patient name, Doctor, Appointment time 
+#### Screenshot
+![Job Dashboard Summary Screenshot](https://raw.githubusercontent.com/binokochumolvarghese/job-dashboard-react-app/public/job-summary-dashboard-ss.png)
+
+#### Tech stack
+- React
+- Firebase
--- src/components/dashboard/Home.jsx
+++ src/components/dashboard/Home.jsx
@@ -25,113 +25,115 @@ const Home = () => {
   useEffect(() => {
     fetchData();
 
-    const interval = setInterval(()=> fetchData(), 30000);
-    return ()=> {
-        clearInterval(interval);
-    }
+    const interval = setInterval(() => fetchData(), 30000);
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return (
     <>
       {error && <p> {error} </p>}
 
-      { !error && 
-
-      <div className="bodyComponent container mx-auto pb-4 pt-5">
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5 ">
-          {data &&
-            data.header.map((header,index) => {
-              return (
-                <Card key={index} props={{ details: header.head, value: header.value }} />
-              );
-            })}
-        </div>
-
-        <div className="grid gap-4 md:grid-cols-3 md:gap-2 xl:grid-cols-3 2xl:gap-2 pt-5">
-          <table className="min-w-full divide-y divide-gray-200 bg-white mt-4 col-span-2">
-            <thead>
-              <tr>
-                <th className="px-6 py-3  text-xl font-medium text-gray-500 text-left">
-                  #
-                </th>
-                <th className="px-6 py-3  text-xl font-medium text-gray-500 text-left">
-                  Name
-                </th>
-                <th className="px-6 py-3  text-xl font-medium text-gray-500 text-right">
-                  Total Jobs
-                </th>
-                <th className="px-6 py-3  text-xl font-medium text-gray-500 text-right">
-                  Completed Jobs
-                </th>
-                <th className="px-6 py-3  text-xl font-medium text-gray-500 text-right">
-                  Pending Jobs
-                </th>
-              </tr>
-            </thead>
-
+      {!error && (
+        <div className="bodyComponent container mx-auto pb-4 pt-5">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5 ">
             {data &&
-              data.details.map((items,index) => {
+              data.header.map((header, index) => {
                 return (
-                  <tbody>
-                    <tr key={items.id}>
-                      <td className="px-6 py-4 whitespace-nowrap text-xl font-medium text-gray-800 text-left">
-                        {items.id}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-xl font-medium text-gray-800 text-left">
-                        {items.EmployeeName}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-xl font-medium text-blue-500 text-right">
-                        {items.TotalJobs}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-xl font-medium text-green-500 text-right">
-                        {items.CompletedJobs}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-xl font-medium text-red-500 text-right">
-                        {items.PendingJobs}
-                      </td>
-                    </tr>
-                  </tbody>
+                  <Card
+                    key={index}
+                    props={{ details: header.head, value: header.value }}
+                  />
                 );
               })}
-          </table>
+          </div>
 
-          <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700 bg-white mt-4">
-            <thead>
-              <tr>
-                <th className="px-4 py-2  text-xl font-medium text-gray-500 text-left">
-                  #
-                </th>
-                <th className="px-4 py-2  text-xl font-medium text-gray-500 text-left">
-                  Job Details
-                </th>
-                <th className="px-4 py-2  text-xl font-medium text-gray-500 text-left">
-                  Employee
-                </th>
-              </tr>
-            </thead>
+          <div className="grid gap-4 md:grid-cols-3 md:gap-2 xl:grid-cols-3 2xl:gap-2 pt-5">
+            <table className="min-w-full divide-y divide-gray-200 bg-white mt-4 col-span-2">
+              <thead>
+                <tr>
+                  <th className="px-6 py-3  text-xl font-medium text-gray-500 text-left">
+                    #
+                  </th>
+                  <th className="px-6 py-3  text-xl font-medium text-gray-500 text-left">
+                    Name
+                  </th>
+                  <th className="px-6 py-3  text-xl font-medium text-gray-500 text-right">
+                    Total Jobs
+                  </th>
+                  <th className="px-6 py-3  text-xl font-medium text-gray-500 text-right">
+                    Pending Jobs
+                  </th>
+                  <th className="px-6 py-3  text-xl font-medium text-gray-500 text-right">
+                    Completed Jobs
+                  </th>
+                </tr>
+              </thead>
 
-            {data &&
-              data.JobDetails.map((jobItems,index) => {
-                return (
-                  <tbody key={jobItems.id}>
-                    <tr>
-                      <td className="px-4 py-2 whitespace-nowrap text-lg font-medium text-gray-800 text-left">
-                        {jobItems.id}
-                      </td>
-                      <td className="px-4 py-2 whitespace-nowrap text-lg font-medium text-gray-800 text-left">
-                        {jobItems.JobName}
-                      </td>
-                      <td className="px-4 py-2 whitespace-nowrap text-lg font-medium text-gray-800 text-left">
-                        {jobItems.Employee}
-                      </td>
-                    </tr>
-                  </tbody>
-                );
-              })}
-          </table>
+              {data &&
+                data.details.map((items, index) => {
+                  return (
+                    <tbody>
+                      <tr key={items.id}>
+                        <td className="px-6 py-4 whitespace-nowrap text-xl font-medium text-gray-800 text-left">
+                          {items.id}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-xl font-medium text-gray-800 text-left">
+                          {items.EmployeeName}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-xl font-medium text-blue-500 text-right">
+                          {items.TotalJobs}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-xl font-medium text-red-500 text-right">
+                          {items.PendingJobs}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-xl font-medium text-green-500 text-right">
+                          {items.CompletedJobs}
+                        </td>
+                      </tr>
+                    </tbody>
+                  );
+                })}
+            </table>
+
+            <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700 bg-white mt-4">
+              <thead>
+                <tr>
+                  <th className="px-4 py-2  text-xl font-medium text-gray-500 text-left">
+                    #
+                  </th>
+                  <th className="px-4 py-2  text-xl font-medium text-gray-500 text-left">
+                    Job Details
+                  </th>
+                  <th className="px-4 py-2  text-xl font-medium text-gray-500 text-left">
+                    Employee
+                  </th>
+                </tr>
+              </thead>
+
+              {data &&
+                data.JobDetails.map((jobItems, index) => {
+                  return (
+                    <tbody key={jobItems.id}>
+                      <tr>
+                        <td className="px-4 py-2 whitespace-nowrap text-lg font-medium text-gray-800 text-left ">
+                          {jobItems.id}
+                        </td>
+                        <td className="px-4 py-2 whitespace-nowrap text-lg font-medium text-gray-800 text-left">
+                          {jobItems.JobName}
+                        </td>
+                        <td className="px-4 py-2 whitespace-nowrap text-lg font-medium text-gray-800 text-left ">
+                          {jobItems.Employee}
+                        </td>
+                      </tr>
+                    </tbody>
+                  );
+                })}
+            </table>
+          </div>
         </div>
-      </div>
-}
+      )}
     </>
   );
 };
